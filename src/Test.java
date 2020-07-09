a4f0bfa77a5d01f414694ee180344251ef5621d3
user <user@LAPTOP-R61MRE4T>
2020-07-09 10:11:10 +0800

    prac4

---
+++ src/Test.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+
+public class Test {
+    public static void main(String []args){
+        
+        Student s1 = new Student();
+        Student s2 = new Student();
+        
+    
+        s1.setName("john");
+        s1.setGPA("3.0"); //5.0 =invalid
+        
+        System.out.println(s2.getName());
+        System.out.println(s2.getGPA());
+        
+       
+        }
+      }        
+    }
+
+
